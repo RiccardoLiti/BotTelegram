dffe5598d9f83e3628b3aaf68b622cad39b25925
RiccardoLiti <43335360+RiccardoLiti@users.noreply.github.com>
2018-09-17 13:15:24 +0200

    Create primocomando.php

---
+++ primocomando.php
@@ -0,0 +1,8 @@
+<?php
+//https://api.telegram.org/bot604459207:AAFBIaumk-YylXub7ZzBPR52k3omNjet0yQ
+$token = "604459207:AAFBIaumk-YylXub7ZzBPR52k3omNjet0yQ";
+$website = "https://api.telegram.org/bot".$token;
+
+
+
+>
